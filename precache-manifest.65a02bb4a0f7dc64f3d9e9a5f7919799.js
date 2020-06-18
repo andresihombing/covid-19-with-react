d25804c5e199b790f77d8a2f568d7deb91c502a7
Andre Sihombing <andrehombing140@gmail.com>
2020-06-18 20:41:52 +0700

    Updates

---
+++ precache-manifest.65a02bb4a0f7dc64f3d9e9a5f7919799.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0357349af324d16a624d76fc80772faf",
+    "url": "/covid-19-with-react/index.html"
+  },
+  {
+    "revision": "fd73846acceeef3fd7bd",
+    "url": "/covid-19-with-react/static/css/main.246f152d.chunk.css"
+  },
+  {
+    "revision": "b7df031a41359b159d08",
+    "url": "/covid-19-with-react/static/js/2.ccae9a30.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/covid-19-with-react/static/js/2.ccae9a30.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fd73846acceeef3fd7bd",
+    "url": "/covid-19-with-react/static/js/main.8072e8fb.chunk.js"
+  },
+  {
+    "revision": "03cf8f55f3e3f0840503",
+    "url": "/covid-19-with-react/static/js/runtime-main.1db55228.js"
+  }
+]);
